3fa8b5d85a48b98ee5f129e3afcfc87a4ceff9b8
Rob <robburson07@gmail.com>
2021-10-17 20:04:14 -0500

    pagination

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,8 +21,10 @@ function createQuiz() {
     }
     // add this question and its answers to the output
     output.push(
-      `<div class="question">${currentQuestion.question}</div>
-                <div class="answers">${answers.join("")}</div>`
+      `<div class="slide">
+      <div class="question">${currentQuestion.question}</div>
+                <div class="answers">${answers.join("")}</div>
+                </div>`
     );
   });
 
@@ -62,6 +64,34 @@ function displayResults() {
   resultsCont.innerHTML = `${numCorrect} out of ${questions.length}`;
 }
 
+function showSlide(n) {
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+    if(currentSlide === 0){
+      previousButton.style.display = 'none';
+    }
+    else{
+      previousButton.style.display = 'inline-block';
+    }
+    if(currentSlide === slides.length-1){
+      nextButton.style.display = 'none';
+      submitButton.style.display = 'inline-block';
+    }
+    else{
+      nextButton.style.display = 'inline-block';
+      submitButton.style.display = 'none';
+    }
+  }
+
+  function showNextSlide() {
+      showSlide(currentSlide+1);
+  }
+
+  function showPreviousSlide() {
+      showSlide(currentSlide-1);
+  }
+
 //variables
 const quizCont = document.getElementById("quiz");
 const resultsCont = document.getElementById("results");
@@ -99,6 +129,17 @@ const questions = [
 //Kick things off
 createQuiz();
 
+//Pagination
+const previousButton = document.getElementById("previous");
+const nextButton = document.getElementById("next");
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+//show the first slide
+showSlide(currentSlide);
+
 //event listeners
 //on submit, show results
 submitBtn.addEventListener("click", displayResults);
+previousButton.addEventListener("click", showPreviousSlide);
+nextButton.addEventListener("click", showNextSlide);
